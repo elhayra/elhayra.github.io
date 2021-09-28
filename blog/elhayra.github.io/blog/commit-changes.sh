4305ece7b9951c1a474bfc538fa6ae4ce03917ca
Elhay <elhay@firstdag.com>
2021-09-28 16:26:43 +0300

    ./commit-changes.sh

---
+++ blog/elhayra.github.io/blog/commit-changes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+commit_msg=$0
+if [$# -eq 0]; 
+    then echo "no commit message was provided. using default"
+    commit_msg="updating blog" 
+fi
+
+cd elhayra.github.io && git pull && cd ..
+
+bundle exec jekyll build
+
+cp -rf _site/* elhayra.github.io/blog
+
+cd elhayra.github.io
+git add .
+git commit -a $commit_msg 
+git push
+
+echo "Done"
+
